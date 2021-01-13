49fcfed18cf4fae0e89c211b5bd7868a52a895ed
subhasishnath45 <subhasishnath45@gmail.com>
2021-01-13 09:22:00 +0530

    3rd Commit

--- src/index.js
+++ src/index.js
@@ -17,17 +17,22 @@ const secondBook = {
 // Our main componwnt
 function BookList(){
 return <section className='booklist'>
-    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+      <p>
+        simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    </Book>
     <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
   </section>;
 }
 // Our Sub Component.
-const Book = (props) => {
+const Book = ({img,title, author, children}) => {
   // console.log(props);
   return (<article>
-          <img src={props.img} />
-          <h1>{props.title}</h1>
-          <h4 style={{color:'#ff0000',fontStyle:'italic',marginTop:'5px'}}>{props.author}</h4>
+          <img src={img} />
+          <h1>{title}</h1>
+          <h4 style={{color:'#ff0000',fontStyle:'italic',marginTop:'5px'}}>{author}</h4>
+          {children}
         </article>);
 }
 ReactDom.render(<BookList/>, document.getElementById('root'));
